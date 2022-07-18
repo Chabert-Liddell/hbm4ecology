7b262c5214fa16f61e720a43b677aff8abaed4cd
Saint-Clair <saint-clair.chabert-liddell@agroparistech.fr>
2022-07-18 17:41:56 +0200

    Building some articles

--- data-raw/history.R
+++ data-raw/history.R
@@ -7,5 +7,27 @@ usethis::use_r("utils")
 devtools::document()
 
 usethis::use_pkgdown()
+usethis::use_github_pages()
+usethis::use_github_action("pkgdown")
 pkgdown::build_site()
+pkgdown::build_articles()
 pkgdown::build_home_index(); pkgdown::init_site();pkgdown::preview_site()
+
+#
+# usethis::use_article(name = "namibian-hake-biomass-production",
+#                      title = "Biomass Production Model: The Namibian Hake Fishery")
+# usethis::use_article(name = "state-space-modeling-of-a-salmon-life-cycle-model",
+#                      title = "State-space Modeling of A. Salmon Life Cycle Model")
+# usethis::use_article(name = "hierarchical-stock-recruitment analysis",
+#                      title = "Hierarchical Stock Recruitment Analysis")
+# usethis::use_article(name = "hierarchical-exchangeable-cmr",
+#                      title = "Hierarchical Exchangeable Binomial Model
+#                      for Capture-Mark-Recapture Data")
+# usethis::use_article(name = "hierarchical-stock-recruitment",
+#                      title = "Hierarchical Stock Recruitment")
+#
+# usethis::use_article(name = "hierarchical-successive-removal",
+#                      title = "Hierarchical Successive Removal")
+# usethis::use_article(name = "hierarchical-succesive-removal-with-habitat-and-time-covariates",
+#                      title = "Hierarchical Model for Successive Removal Data
+#                      with Habitat and Time Covariates")
